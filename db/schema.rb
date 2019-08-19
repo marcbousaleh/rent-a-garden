5c25f191e9d32e662c24413a750944cba8c95738
ghita tahiri <ghitatahiri8@gmail.com>
2019-08-19 16:48:57 +0100

    fixed migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_19_144704) do
-
+ActiveRecord::Schema.define(version: 2019_08_19_143333) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,7 +51,6 @@ ActiveRecord::Schema.define(version: 2019_08_19_144704) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
 
   add_foreign_key "bookings", "gardens"
   add_foreign_key "bookings", "users", column: "renter_id"
