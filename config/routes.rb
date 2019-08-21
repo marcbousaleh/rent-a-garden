0ac59e5ac8d588cc87a4f86ecfe935e7490f6bb8
Marc BouSaleh <marcabisaleh@gmail.com>
2019-08-21 09:05:37 +0100

    basic pundit for garden

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   get 'payment-confirmation', to: 'pages#payment_confirmation'
-  resources :gardens, only: [:index, :new, :create, :show, :destroy]
+  resources :gardens, only: [:index, :new, :create, :show, :edit, :update, :destroy]
   resources :bookings, only: [:index, :new, :create]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
