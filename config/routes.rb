0a2eab01c275fb5f23f8f45d202d4bf3d54d41c7
ghita tahiri <ghitatahiri8@gmail.com>
2019-08-21 11:52:29 +0100

    My gardens section, a little of styling too

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'payment-confirmation', to: 'pages#payment_confirmation'
   resources :gardens, only: [:index, :new, :create, :show, :edit, :update, :destroy]
   resources :bookings, only: [:index, :new, :create]
+  get 'my_gardens', to: 'gardens#my_gardens'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
