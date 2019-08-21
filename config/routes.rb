1a6b27bc7fe21b1c5e763323f83a29006692469a
Melissa Connors <melconnors04@yahoo.com>
2019-08-21 16:23:17 +0100

    made changes to payment confirmation

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  get 'payment-confirmation', to: 'pages#payment_confirmation'
 
   resources :gardens, only: [:index, :new, :create, :show, :edit, :update, :destroy] do
     resources :bookings, only: [:new, :create]
   end
-  resources :bookings, only: [:index]
+  resources :bookings, only: [:index] do
+    get 'payment-confirmation', to: 'pages#payment_confirmation'
+  end
   get 'my_gardens', to: 'gardens#my_gardens'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
