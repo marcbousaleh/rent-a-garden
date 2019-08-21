7465429be1f68f80716dc346857fe62a82a491ac
Demi Ogun <demiogun1@gmail.com>
2019-08-21 13:23:09 +0100

    need to merge

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -4,5 +4,9 @@ class Garden < ApplicationRecord
 
   validates :price, :capacity, :address, :description, :owner, :title, presence: true
   validates :address, uniqueness: true
+
   mount_uploader :photo, PhotoUploader
+
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
