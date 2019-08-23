1586fbc67e0927295c1938441e72b4d1f1e16538
Marc BouSaleh <marcabisaleh@gmail.com>
2019-08-23 13:29:58 +0100

    reservations

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -12,12 +12,13 @@ class BookingsController < ApplicationController
 
   def create
     @booking = Booking.new(booking_params)
-    @booking.garden = Garden.find(params[:garden_id])
+    @garden = Garden.find(params[:garden_id])
+    @booking.garden = @garden
     authorize @booking
-    @booking.renter = current_user
-    time = (@booking.end_date - @booking.start_date) / 3600
-    @booking.price = @booking.garden.price * time
-    @booking.status = 'available'
+
+    check_if_reserved(@garden, @booking)
+
+    add_booking_attributes(@booking)
 
     if @booking.save
       redirect_to bookings_path
@@ -27,6 +28,23 @@ class BookingsController < ApplicationController
   end
 
   private
+
+  def check_if_reserved(garden, booking)
+    garden.bookings.each do |reservation|
+      if ((reservation.start_date < booking.start_date) && (reservation.end_date > booking.start_date)) || ((reservation.start_date < booking.end_date) && (reservation.end_date > booking.end_date)) || ((reservation.start_date > booking.start_date) && (reservation.end_date < booking.end_date))
+        flash.now[:alert] = "This garden is already booked on this date"
+        render :new
+      end
+      break
+    end
+  end
+
+  def add_booking_attributes(booking)
+    booking.renter = current_user
+    time = (booking.end_date - booking.start_date) / 3600
+    booking.price = booking.garden.price * time
+    booking.status = 'available'
+  end
 
   def booking_params
     params.require(:booking).permit(:start_date, :end_date, :garden_id)
